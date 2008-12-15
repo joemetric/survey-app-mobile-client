7a1e5fdca4a30b4ff20a59f00d2ffa0fdc1eabd3
Joe O'Brien <joe@theedgecase.com>
2008-12-15 11:08:04 -0500

    it helps if you actually add things to git

---
+++ iphone/Classes/QuestionList.h
@@ -0,0 +1,18 @@
+//
+//  QuestionList.h
+//  JoeMetric
+//
+//  Created by Joseph OBrien on 12/15/08.
+//  Copyright 2008 EdgeCase, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface QuestionList : NSObject {
+    NSArray *questions;
+}
+
+-(NSArray *)questions;
+
+@end
---
+++ iphone/Classes/QuestionList.m
@@ -0,0 +1,14 @@
+//
+//  QuestionList.m
+//  JoeMetric
+//
+//  Created by Joseph OBrien on 12/15/08.
+//  Copyright 2008 EdgeCase, LLC. All rights reserved.
+//
+
+#import "QuestionList.h"
+
+
+@implementation QuestionList
+
+@end
