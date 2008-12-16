617060d3b17fc5b6b5b674d8dd0c1a33b622e670
Jerry Nummi <jerry@theedgecase.com>
2008-12-16 14:36:19 -0500

    Added mockups controller.

---
+++ rails/app/controllers/mockups_controller.rb
@@ -0,0 +1,28 @@
+class MockupsController < ApplicationController
+  layout :select_layout
+
+  # To add a mockup, just create a new *.html.erb file in app/views/mockups.
+  # It will then be accessible from /mockups and (directly) /mockups/my_mockup
+  # You can optionally define actions for each mockup if you want to use a
+  # layout or use instance variables
+
+  # def my_mockup
+  #   layout 'application'
+  #   @some_obj = SomeObject.new
+  # end
+
+  def index
+    files = Dir.glob "#{Rails.root}/app/views/mockups/[^_]*.erb"
+    @pages = files.map { |f| f.scan(/(\w+)\./).flatten.first }
+  end
+
+  private
+
+    def select_layout
+      @layout || false
+    end
+
+    def layout(name)
+      @layout = name
+    end
+end
