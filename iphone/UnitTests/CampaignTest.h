5803dc2ca518414f228f96645b04ce9a0ac863b9
Scott Barron <scott@elitists.net>
2009-01-06 11:51:52 -0500

    skeleton Campaign test, add to targets

---
+++ iphone/UnitTests/CampaignTest.h
@@ -0,0 +1,17 @@
+//
+//  CampaignTest.h
+//  JoeMetric
+//
+//  Created by Scott Barron on 01/06/09.
+//  Copyright 2009 EdgeCase, LLC. All rights reserved.
+//
+
+#import "GTMSenTestCase.h"
+#import "Campaign.h"
+
+
+@interface CampaignTest : GTMTestCase {
+
+}
+
+@end
