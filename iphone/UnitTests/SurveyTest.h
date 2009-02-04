28e251898e3663989322c194b17f63be08486aea
Scott Barron <scott@elitists.net>
2009-02-04 13:43:11 -0500

    complete campaign => survey rename in the tests

---
+++ iphone/UnitTests/SurveyTest.h
@@ -0,0 +1,17 @@
+//
+//  SurveyTest.h
+//  JoeMetric
+//
+//  Created by Scott Barron on 01/06/09.
+//  Copyright 2009 EdgeCase, LLC. All rights reserved.
+//
+
+#import "GTMSenTestCase.h"
+#import "Survey.h"
+
+
+@interface SurveyTest : GTMTestCase {
+
+}
+
+@end
--- iphone/UnitTests/SurveyTest.m
+++ iphone/UnitTests/SurveyTest.m
@@ -1,17 +1,18 @@
 //
-//  CampaignTest.h
+//  SurveyTest.m
 //  JoeMetric
 //
 //  Created by Scott Barron on 01/06/09.
 //  Copyright 2009 EdgeCase, LLC. All rights reserved.
 //
 
-#import "GTMSenTestCase.h"
-#import "Campaign.h"
+#import "SurveyTest.h"
 
+@implementation SurveyTest
 
-@interface CampaignTest : GTMTestCase {
-
+- (void) testSurveyClassExists
+{
+    [[Survey alloc] init];
 }
 
 @end
