4d58e1fd62ec7bad297fd2780241d3ca0d73e08c
Scott Barron <scott@elitists.net>
2009-02-13 09:32:09 -0500

    how did at ^K get inserted?  Better yet - how did it *compile*?

--- iphone/Classes/JoeMetricAppDelegate.h
+++ iphone/Classes/JoeMetricAppDelegate.h
@@ -16,6 +16,6 @@
 
 @property (nonatomic, retain) IBOutlet UIWindow *window;
 @property (nonatomic, retain) IBOutlet UITabBarController *tabBarController;
-@property (nonatomic, retain) IBOutlet UINavigationController *navigationController;
+@property (nonatomic, retain) IBOutlet UINavigationController *navigationController;
 
 @end
