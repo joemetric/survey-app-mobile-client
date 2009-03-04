142e542db8da83745ea04a8519362c802c07a5f7
Jon Distad <jon@theedgecase.com>
2009-03-03 22:07:32 -0500

    renamed sam to user

--- rails/app/controllers/sessions_controller.rb
+++ rails/app/controllers/sessions_controller.rb
@@ -1,7 +1,7 @@
 # This controller handles the login/logout function of the site.  
 class SessionsController < ApplicationController
-  # Be sure to include AuthenticationSystem in Application Controller instead
-  include AuthenticatedSystem
+
+  skip_before_filter :login_required
 
   # render new.rhtml
   def new
@@ -9,13 +9,13 @@ class SessionsController < ApplicationController
 
   def create
     logout_keeping_session!
-    sam = Sam.authenticate(params[:login], params[:password])
-    if sam
+    user = User.authenticate(params[:login], params[:password])
+    if user
       # Protects against session fixation attacks, causes request forgery
       # protection if user resubmits an earlier form using back
       # button. Uncomment if you understand the tradeoffs.
       # reset_session
-      self.current_sam = sam
+      self.current_user = user
       new_cookie_flag = (params[:remember_me] == "1")
       handle_remember_cookie! new_cookie_flag
       redirect_back_or_default('/')
--- rails/app/controllers/users_controller.rb
+++ rails/app/controllers/users_controller.rb
@@ -1,23 +1,23 @@
-class SamsController < ApplicationController
+class UsersController < ApplicationController
   # Be sure to include AuthenticationSystem in Application Controller instead
   include AuthenticatedSystem
   
 
   # render new.rhtml
   def new
-    @sam = Sam.new
+    @user = User.new
   end
  
   def create
     logout_keeping_session!
-    @sam = Sam.new(params[:sam])
-    success = @sam && @sam.save
-    if success && @sam.errors.empty?
+    @user = User.new(params[:user])
+    success = @user && @user.save
+    if success && @user.errors.empty?
             # Protects against session fixation attacks, causes request forgery
       # protection if visitor resubmits an earlier form using back
       # button. Uncomment if you understand the tradeoffs.
       # reset session
-      self.current_sam = @sam # !! now logged in
+      self.current_user = @user # !! now logged in
       redirect_back_or_default('/')
       flash[:notice] = "Thanks for signing up!  We're sending you an email with your activation code."
     else
