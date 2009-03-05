22dcd286049072b1f6eb934fddfec9bef82282c6
Jon Distad <jon@theedgecase.com>
2009-03-04 20:10:53 -0500

    [Matt Jon] added associations and validations to location

--- rails/app/models/location.rb
+++ rails/app/models/location.rb
@@ -1,2 +1,5 @@
 class Location < ActiveRecord::Base
+  belongs_to :owner, :polymorphic => true 
+
+  validates_presence_of :name, :address_line1, :city, :state, :zipcode, :country, :owner
 end
--- rails/test/unit/location_test.rb
+++ rails/test/unit/location_test.rb
@@ -2,8 +2,8 @@ require File.dirname(__FILE__) + '/../test_helper'
 
 class LocationTest < ActiveSupport::TestCase
 
-  context "Associations" do
-    should_belong_to :owner
-  end
+  should_belong_to :owner
+
+  should_require_attributes :name, :address_line1, :city, :state, :zipcode, :country, :owner
 
 end
