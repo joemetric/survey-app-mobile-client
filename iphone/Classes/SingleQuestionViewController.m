8c5f42a97819e990f2b91d418b80e6f9d3ffb66b
Scott Barron <scott@elitists.net>
2009-03-05 09:48:57 -0500

    fix crash on question answer screen load

--- iphone/Classes/SingleQuestionViewController.m
+++ iphone/Classes/SingleQuestionViewController.m
@@ -12,22 +12,13 @@
 
 @implementation SingleQuestionViewController
 
--(void)animateTheTransition:(UIViewController *)controller {
-    [UIView beginAnimations:nil context:NULL];
-    [UIView setAnimationDuration:1];
-    [UIView setAnimationTransition:UIViewAnimationTransitionFlipFromRight forView:self.navigationController.view cache:YES];
-    
-    [UIView commitAnimations];
-}
-
 -(IBAction)answerQuestion:(id)sender {   
     NSLog(@"Answering the question");
     SurveyQuestionViewController *sqvc = [[SurveyQuestionViewController alloc] 
-                                          initWithNibName:@"SurveyQuestionView" 
-                                                   bundle:nil 
-                                                 question:self.selectedQuestion];
+                                             initWithNibName:@"SurveyQuestionView" 
+                                             bundle:nil 
+                                             question:self.selectedQuestion];
     [self.navigationController pushViewController:sqvc animated:YES];    
-    [self animateTheTransition:sqvc];
  
     [sqvc release];
 }
