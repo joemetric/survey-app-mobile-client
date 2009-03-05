aefa851732885c90dbec3424f82c3c3600ce28ec
Jon Distad <jon@theedgecase.com>
2009-03-04 20:15:30 -0500

    [Matt Jon] added demographics module and app/mixins to the load path

---
+++ rails/app/mixins/demographic.rb
@@ -0,0 +1,5 @@
+module Demographic
+  
+  
+
+end
---
+++ rails/test/unit/demographic_test.rb
@@ -0,0 +1,9 @@
+require File.dirname(__FILE__) + '/../test_helper'
+
+class DemographicModel
+  include Demographic
+end
+
+class DemographicTest < ActiveSupport::TestCase
+end
+
