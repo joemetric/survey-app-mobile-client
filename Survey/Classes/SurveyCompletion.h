b5b39de58c0787aa7239d53a3c0ebec5deb12964
dev1@gniyes.com <dev1@gniyes.com>
2010-02-03 19:10:04 +0530

    added class to show the Thank you page when survey finished

---
+++ Survey/Classes/SurveyCompletion.h
@@ -0,0 +1,47 @@
+//
+//  SurveyCompletion.h
+//  Survey
+//
+//  Created by mobile06 on 22/01/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class BrowseController,Survey;
+@interface SurveyCompletion : UIViewController {
+	UIButton	*charityOrganizationImage1;
+	UIButton	*charityOrganizationImage2;
+	UIButton	*charityOrganizationImage3;
+	UIButton	*charityOrganizationImage4;
+	UIButton	*charityOrganizationImage5;
+	
+	UILabel		*earnedAmountLabel;
+	UILabel		*donationAmountToCharityLabel;
+	UILabel		*earnedAmountMessageLabel;
+	UILabel		*genourisityLabel;
+	NSString	*donatedPercernatge;
+	NSInteger   selectedPercentage;
+	BrowseController *browseController;
+	NSNumber *amt;
+	float percetageCheck;
+}
+
+@property(nonatomic,retain) IBOutlet UIButton	*charityOrganizationImage1;
+@property(nonatomic,retain) IBOutlet UIButton	*charityOrganizationImage2;
+@property(nonatomic,retain) IBOutlet UIButton	*charityOrganizationImage3;
+@property(nonatomic,retain) IBOutlet UIButton	*charityOrganizationImage4;
+@property(nonatomic,retain) IBOutlet UIButton	*charityOrganizationImage5;
+@property(nonatomic,retain) IBOutlet UILabel	*earnedAmountLabel;
+@property(nonatomic,retain) IBOutlet UILabel	*donationAmountToCharityLabel; 
+@property(nonatomic,retain) IBOutlet UILabel	*earnedAmountMessageLabel; 
+@property(nonatomic,retain) IBOutlet UILabel	*genourisityLabel; 
+
+@property(nonatomic,retain) BrowseController	*browseController;
+@property(nonatomic,retain) NSString			*donatedPercernatge;
+
+
+
+- (IBAction) saveAmountAndGotoMainScreenClicked:(UIButton*)image;
+- (float) earnedAmountByUser:(float) percentage;
+@end
